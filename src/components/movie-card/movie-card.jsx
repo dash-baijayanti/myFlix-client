94dd1c8a2954a44c850a56074aeaa071d32a33c8
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-07-31 10:01:01 +0200

    document updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 export const MovieCard = ({ movieData, onMovieClick }) => {
   return (
     <div
@@ -8,4 +9,23 @@ export const MovieCard = ({ movieData, onMovieClick }) => {
       {movieData.Title}
     </div>
   );
+};
+
+PropTypes.MovieCard = {
+  movieData: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birthYear: PropTypes.string.isRequired,
+      deathYear: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  onBookClick: PropTypes.func.isRequired,
 };
